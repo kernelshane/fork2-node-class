65894ca8d108a89c16f77c1263bea86ca7c17096
ShaneH <shane@mbp.local>
2014-05-03 17:25:12 +0800

    inheritance

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = function Class(obj) {
+module.exports = function Class(obj, parent) {
     var constructor;
     if (obj.hasOwnProperty('initialize')) {
         constructor = obj.initialize;
@@ -8,6 +8,13 @@ module.exports = function Class(obj) {
     for (var key in obj) {
         if (key != 'initialize' && typeof obj[key] == 'function') {
             constructor.prototype[key] = obj[key];
+        }
+    }
+    if (parent) {
+        for (var key in parent.prototype) {
+            if (typeof parent.prototype[key] == 'function') {
+                constructor.prototype[key] = parent.prototype[key];
+            }
         }
     }
     return constructor;
