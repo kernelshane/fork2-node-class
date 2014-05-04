bc0af0ba5f7c9bd230b7ddc6d54355bc6b7501a3
ShaneH <shane@mbp.local>
2014-05-04 15:53:40 +0800

    super super

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
-module.exports = function Class(obj, parent) {
-    var constructor = obj.initialize || function() {};
-    var ctor = function() {};
+module.exports = function(obj, parent) {
+    var func = obj.initialize || function() {};
     parent = parent || Object;
-    ctor.prototype = parent.prototype;
-    constructor.prototype = new ctor();
-    constructor.prototype.constructor = constructor;
 
+    function ctor() {
+        this.constructor = func;
+    };
+
+    ctor.prototype = parent.prototype;
+    func.prototype = new ctor();
+
+    func.__super__ = parent;
+
+    var _super = parent.prototype;
     for(var key in obj) {
-        if(obj.hasOwnProperty(key) && key != 'initialize') {
-            constructor.prototype[key] = obj[key];
+        if(key != 'initialize') {
+            if(typeof obj[key] == 'function' && typeof _super[key] == 'function') {
+                func.prototype[key] = (function(name, fn) {
+                    return function() {
+                        var tmp = this._super;
+                        this._super = _super[key];
+                        var ret = fn.apply(this, arguments);
+                        this._super = tmp;
+                        return ret;
+                    }
+                } (key, obj[key]));
+            }
+            else {
+                func.prototype[key] = obj[key];
+            }
         }
     }
 
-    constructor.__super__ = parent;
+    var current_class = func;
+    func.prototype.super = function(name) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        var tmp = current_class;
+        current_class = current_class.__super__;
+        var ret = current_class.prototype[name].apply(this, args);
+        current_class = tmp;
+        return ret;
+    };
 
-    return constructor;
+    return func;
 };
-
-
